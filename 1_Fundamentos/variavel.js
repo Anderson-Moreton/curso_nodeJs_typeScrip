705065ebaffab3819417b5a4f5ce6fc8dff65284
Anderson Moreton <anderson.moreton@gmail.com>
2025-12-02 17:23:09 +0000

    Curso de JavaScript para relembrar fundamentos

---
+++ 1_Fundamentos/variavel.js
@@ -0,0 +1,22 @@
+// let // Ele é utilicado globalmente
+// var // Ele é utilizado dentro de um escopo
+// const // Ele é utilizado dentro de um escopo, porém podemos armazenar dados apenas 1X
+
+var cachorro = "Duda"
+console.log(cachorro)
+
+cachorro = "Ozzy"
+console.log(cachorro)
+
+
+let lanche = "X-Salada"
+console.log(lanche)
+
+lanche = "X-Bacon"
+console.log(lanche)
+
+const nome = "Anderson"
+console.log(nome)
+
+// nome = "Thiago"  //ERRO, connst não pode ser alterada depois de declarada!
+// console.log(nome)
