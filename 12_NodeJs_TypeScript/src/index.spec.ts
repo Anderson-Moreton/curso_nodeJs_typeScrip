570823782b56952d3bfbbef7c4cb3f77f0c7477e
Anderson Moreton <anderson.moreton@gmail.com>
2025-12-12 02:47:00 +0000

    Node JS and TypeScript

---
+++ 12_NodeJs_TypeScript/src/index.spec.ts
@@ -0,0 +1,11 @@
+// import { data } from "./index";
+
+describe("File jest - Test", () => {
+    test('Test', () => {
+        expect(1).toBe(1)
+    });
+
+    // test('Test', () => {
+    //     expect(data).toBe("Anderson Moreton")
+    // });    
+}); 
---
+++ 12_NodeJs_TypeScript/src/index.ts
@@ -0,0 +1,6 @@
+// export const data = "Anderson Moreton";
+
+// console.log(data);
+
+console.log(123);
+console.log(1234);
